c0f1c777b7d3a8b8d29cbd123fdae6398aed5da2
Kyle Errold Dula <92439099+KEDula2001@users.noreply.github.com>
2023-02-25 23:02:38 +0800

    changes to the rotc account

--- Modules/DocumentRequest/Views/requests/approval.php
+++ Modules/DocumentRequest/Views/requests/approval.php
@@ -84,21 +84,21 @@
                                                 <?php endif; ?>
                                             </td>
                                             <td class="text-center">
-                                                <?php if(session()->get('office') == "ROTC Office"):?>
+                                                <?php if(session()->get('office') == "ROTC"):?>
                                                     <button 
-                                                        <?php if($request['ROTC Office'] == 0):?>
+                                                        <?php if($request['rotc'] == 0):?>
                                                             onclick="approvePerOffices('/approval/apply-approval/edit/', 3, <?=$request['request_id']?>, <?=$request['id']?>, '<?=$request['slug']?>', '/approval');" 
                                                         <?php endif;?>
-                                                        class="btn <?=$request['roROTC Officetc']== 0?'btn-warning':'btn-success'?> btn-sm rounded-pill"  
+                                                        class="btn <?=$request['rotc']== 0?'btn-warning':'btn-success'?> btn-sm rounded-pill"  
                                                     >
-                                                        <?php if($request['ROTC Office'] == 0):?>
+                                                        <?php if($request['rotc'] == 0):?>
                                                             Pending
                                                         <?php else:?>
                                                             Cleared
                                                         <?php endif;?>
                                                     </button>
                                                 <?php else: ?>
-                                                    <?php if($request['ROTC Office'] == 0):?>
+                                                    <?php if($request['rotc'] == 0):?>
                                                         <span class="badge rounded-pill bg-warning">Pending</span>
                                                     <?php else:?>
                                                         <span class="badge rounded-pill bg-success">Cleared</span>
